40f75905f3abf86bec1879016e7a4dd5ca18e57e
SankeerthanaSuddala <suddalakeerthana@gmail.com>
2023-08-09 14:19:21 +0530

    Added help details in index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { Command } = require("commander");
 const query=require("./controllers/queries")
 const dataImportFunction = require("./controllers/importData");
+const { default: mongoose } = require("mongoose");
 const program = new Command();
 program
   .version("1.0.0")
   .description("University Ranking details")
   .option("-i, --imp <path>", "Importing the data")
-  .option("-q,--query <param...>")
+  .option("-q,--query <param...>", "for top universities information\nEnter: columnName limit country ")
   .parse(process.argv);
 const options = program.opts();
 if (options.imp) {
@@ -18,4 +19,8 @@ if(options.query){
   const limit=parseInt(options.query[1])
   const country=options.query[2]
   query(columnName,limit,country);
+}
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+  mongoose.connection.close();
 }
